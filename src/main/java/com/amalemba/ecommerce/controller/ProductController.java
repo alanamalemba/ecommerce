3ccf2e510a0bd31353eec8fbbf8d9dd147b844db
Alan Amalemba <alanlsamalemba@gmail.com>
2025-05-08 16:51:32 +0300

    fetch an image

--- src/main/java/com/amalemba/ecommerce/controller/ProductController.java
+++ src/main/java/com/amalemba/ecommerce/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.amalemba.ecommerce.model.Product;
 import com.amalemba.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -40,16 +41,22 @@ public class ProductController {
     }
 
     @PostMapping("/products")
-    public ResponseEntity<?> addProduct(
-            @RequestPart Product product,
-            @RequestPart MultipartFile imageFile) {
-
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
         try {
             Product savedProduct = service.addProduct(product, imageFile);
-            return new ResponseEntity<>(savedProduct,HttpStatus.CREATED);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping("/products/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId) {
+
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok().contentType(MediaType.valueOf(product.getImageType())).body(imageFile);
     }
 
 }
