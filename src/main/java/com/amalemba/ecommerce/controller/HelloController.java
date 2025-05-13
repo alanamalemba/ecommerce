cc7f571692c346f0f12eaf3dd92ea9ad83e200ca
Alan Amalemba <alanlsamalemba@gmail.com>
2025-05-13 12:02:59 +0300

    add custom login

--- src/main/java/com/amalemba/ecommerce/controller/HelloController.java
+++ src/main/java/com/amalemba/ecommerce/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.amalemba.ecommerce.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @RequestMapping("/")
-    public String greet() {
-        return "Hello Alan!";
+    public String greet(HttpServletRequest request) {
+
+        return "Hello Alan! " + request.getSession().getId();
     }
 }
